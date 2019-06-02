f96d3dc5e1cb771e98d7a517a8587e88c1036809
Artit Attasiri <artit.atta@gmail.com>
2019-06-02 22:49:59 +0700

    set container background

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Basic Flutter',
+      title: "Container widget",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Home"),
+          title: Text("Container widget Example"),
         ),
-        body: Center(
-          child: Text(
-            "Welcome to main page",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-        ),
-      ),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+        body: Container(color: Colors.blueGrey, ),
       ),
     );
   }
