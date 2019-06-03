474149a97a944d8091126eae73354b80ad9ca317
Artit Attasiri <artit.atta@gmail.com>
2019-06-03 09:57:07 +0700

    row

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      title: "Row & Column widget",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Row & Column widget Example"),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          textBaseline: TextBaseline.alphabetic,
+          textDirection: TextDirection.rtl,
+          children: <Widget>[
+            FlutterLogo(),
+            Text(
+              "Column 1",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Column 2",
+              style: TextStyle(fontSize: 15),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
