32d9e492a9fc6b10bf0609f82cbb4d231496c80f
Artit Attasiri <artit.atta@gmail.com>
2019-06-06 22:50:29 +0700

    show Stack Layout

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Grid View",
+      title: "Stack Layout",
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Grid View Example"),
-          ),
-          body: GridView.extent(
-            maxCrossAxisExtent: 150,
-            padding: EdgeInsets.all(8),
-            children: _buildGridList(20),
-          )),
-    );
-  }
-
-  List<Card> _buildGridList(int i) {
-    return List.generate(
-        i,
-        (index) => Card(
-              child: Image.network(
-                "http://hobby-play.com/wp-content/uploads/2016/08/ban160456.jpg",
-                fit: BoxFit.cover,
+        appBar: AppBar(
+          title: Text("Stack Layout Example"),
+        ),
+        body: Stack(
+          alignment: Alignment(0, 0.8),
+          children: <Widget>[
+            Image.network(
+              "https://dog-vs-cat.com/wp-content/uploads/2018/05/nnf2.jpg",
+              fit: BoxFit.cover,
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(12, 4, 12, 4),
+              decoration: BoxDecoration(
+                  color: Colors.black45,
+                  borderRadius: BorderRadius.circular(4)),
+              child: Text(
+                "alaskan",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white),
               ),
-            ));
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
